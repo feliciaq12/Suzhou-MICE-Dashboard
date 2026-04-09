1e866c75c426114de38758bcf657948e5aedf327
feliciaq12 <feliciaq12@gmail.com>
2026-04-09 15:37:57 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+fetch("data.json")
+  .then(res => res.json())
+  .then(events => {
+
+    // stats
+    document.getElementById("total").innerText = events.length;
+
+    const now = new Date();
+    const thisMonth = events.filter(e => new Date(e.start).getMonth() === now.getMonth()).length;
+    document.getElementById("month").innerText = thisMonth;
+
+    const upcoming = events.filter(e => new Date(e.start) > now).length;
+    document.getElementById("upcoming").innerText = upcoming;
+
+    const sectors = [...new Set(events.map(e => e.sector))];
+    document.getElementById("sectors").innerText = sectors.length;
+
+    // sector chart
+    const sectorCount = {};
+    events.forEach(e => sectorCount[e.sector] = (sectorCount[e.sector] || 0) + 1);
+
+    new Chart(document.getElementById("sectorChart"), {
+      type: "pie",
+      data: {
+        labels: Object.keys(sectorCount),
+        datasets: [{ data: Object.values(sectorCount) }]
+      }
+    });
+
+    // trend chart
+    const trend = {};
+    events.forEach(e => {
+      const m = e.start?.slice(0,7);
+      trend[m] = (trend[m] || 0) + 1;
+    });
+
+    new Chart(document.getElementById("trendChart"), {
+      type: "line",
+      data: {
+        labels: Object.keys(trend),
+        datasets: [{ data: Object.values(trend) }]
+      }
+    });
+
+    // venue chart
+    const venues = {};
+    events.forEach(e => venues[e.venue] = (venues[e.venue] || 0) + 1);
+
+    new Chart(document.getElementById("venueChart"), {
+      type: "bar",
+      data: {
+        labels: Object.keys(venues).slice(0,10),
+        datasets: [{ data: Object.values(venues).slice(0,10) }]
+      }
+    });
+
+    // tables
+    const table = document.getElementById("eventsTable");
+    table.innerHTML = events.map(e =>
+      `<tr><td>${e.title}</td><td>${e.venue}</td><td>${e.start}</td></tr>`
+    ).join("");
+
+  });
